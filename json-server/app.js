87aef783eed43fb642db38d1c0091786c642318a
Madhav Goyal <57744109+MadhavGoyal-6@users.noreply.github.com>
2022-07-15 01:31:44 +0530

    Add Json-server files

---
+++ json-server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require("cors")
+const app = express()
+const PORT = 3000
+
+const offerRouter = require("./routes/Offers")
+const answerRouter = require("./routes/Answers")
+const userRouter = require("./routes/Users")
+
+function logger(req, res, next) {
+    console.log(req.url)
+    next()
+}
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use(logger)
+// app.use("/user", userRouter)
+app.use("/offers", offerRouter)
+app.use("/answers", answerRouter)
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`))
---
+++ json-server/routes/Answers.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { createDoc, getDoc } = require("../functions/docWriter")
+const router = express.Router()
+
+router.post("/new", (req, res) => {
+    try {
+        createDoc(req.body, "Answers");
+        res.status(200).json({ success: true })
+    } catch (error) {
+        res.status(500).json({ success: false })
+    }
+})
+
+router.post("/get", (req, res) => {
+    let read = getDoc(req.body, "Answers")
+    if (read.result) {
+        res.status(200).json(read)
+    } else {
+        res.status(404).json(read)
+    }
+})
+
+module.exports = router;
